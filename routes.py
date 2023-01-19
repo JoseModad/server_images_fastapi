42afd87b4ffa05a8f11740b14226ba038cb65ce5
turko2373 <turko1073@gmail.com>
2023-01-18 22:59:02 -0300

    path delete

--- routes.py
+++ routes.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, UploadFile, File
+from fastapi.responses import FileResponse, JSONResponse
 
-from os import getcwd
+from os import getcwd, remove
 
 router = APIRouter()
 
@@ -11,3 +12,26 @@ async def upload_file(file: UploadFile = File(...)):
         myfile.write(content)
         myfile.close()
     return "Success"
+
+@router.get("/file/{name_file}")
+def get_file(name_file: str):
+    return FileResponse(getcwd() + "/" + name_file)
+
+@router.get("/download/{name_file}")
+def download_file(name_file: str):
+    return FileResponse(getcwd() + "/" + name_file, media_type="aplication/octet-stream", filename = name_file)
+
+@router.delete("/delete/{name_file}")
+def delete_file(name_file: str):
+    try:
+        remove(getcwd() + "/" + name_file)
+        return JSONResponse(content={
+            "removed": True,            
+        }, status_code = 200)
+    except FileNotFoundError:
+        return JSONResponse(content={
+            "removed": False,
+            "message": "File not found"
+        }, status_code = 404)
+    
+    
